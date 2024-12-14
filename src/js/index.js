448153e461095790484e28cdb33a9636a217e779
Егор Половко <skelletrise@gmail.com>
2024-12-15 02:51:36 +0700

    section services complete

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+import '../scss/_mixins.scss'
+import '../scss/_media.scss'
+import '../scss/_normalize.scss'
+import '../scss/variables.scss'
+import '../scss/_utils.scss'
+import '../scss/_fonts.scss'
+import '../scss/_globals.scss'
+import '../scss/purple-button.scss'
+import '../scss/blocks/header.scss'
+import '../scss/blocks/menu.scss'
+import '../scss/blocks/services.scss'
+
+console.log('Works!')
